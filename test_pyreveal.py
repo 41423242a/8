8a4009a677c035af748f750af00b87a09f03f995
41423242 <hyf0960714@gmail.com>
2025-10-02 23:28:56 +0800

    更新

--- test_pyreveal.py
+++ test_pyreveal.py
@@ -7,21 +7,8 @@ presentation = PyReveal(title="My Presentation", theme="white", transition="slid
 bg_image = ImageBackground(image_url=bg_image_path)
 
 # 加入幻燈片
-presentation.add_slide(
-    title="第8組報告",
-    content="成員:6、7、12、13、19、42",
-    background=bg_image
-)
-presentation.add_slide(
-    title="什麼是 Git？",
-    content="""
-- 分散式版本控制系統
-- 可追蹤檔案變化
-- 協作工具
-""",
-    background=bg_image
-)
-
+presentation.add_slide(title="第8組報告",content="成員:6、7、12、13、19、42",background=bg_image)
+presentation.add_slide(title="什麼是 Git？",content=""" - 分散式版本控制系統 - 可追蹤檔案變化 - 協作工具""", background=bg_image)
 presentation.add_slide("""如何使用 Git？
 1. git init 建立版本庫
 2. git add 加入檔案
@@ -33,7 +20,6 @@ presentation.add_slide("""為什麼要用 Git？
 - 保留歷史紀錄
 - 可回溯版本
 """, background=bg_image)
-presentation.add_slide("1234", background=bg_image)
 presentation.add_slide("1234", background=bg_image)
 presentation.add_slide("謝謝大家", background=bg_image)
 
